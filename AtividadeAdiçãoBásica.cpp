e2d6d0921bd37972bb41764edf22a9e98b8abd11
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2023-10-23 15:56:18 -0300

    Add files via upload

---
+++ AtividadeAdiçãoBásica.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A ;
+    int B ;
+    int resultado;
+    
+    std::cout<<"Digite o primeiro numero"<<endl;
+    std::cin>> A ;
+    
+    std::cout<<"Digite o segundo numero"<<endl;
+    std::cin>> B;
+    
+    resultado=A+B;
+    
+    std::cout<<resultado;
+
+
+    return 0;
+}
