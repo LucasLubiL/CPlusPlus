0caa9f246dc4cb84907ac3d978d82fa2e06fdee4
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2023-10-23 16:17:04 -0300

    Add files via upload

---
+++ ÁreaDoCírculo.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+double R;
+double A;
+    
+    
+    std::cout<<"Digite o valor do Raio"<<std::endl;
+    std::cin>>R; 
+    
+    A=(R*R)*3.14159;
+    
+    std::cout<<"A área do círculo é de "<<A;
+
+    return 0;
+}
