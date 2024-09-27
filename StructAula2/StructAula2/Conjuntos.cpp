3ae17c901d9ea4a189b42df4cea7bb6a7e694794
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-09-27 17:29:27 -0300

    Update Conjuntos.cpp

--- StructAula2/StructAula2/Conjuntos.cpp
+++ StructAula2/StructAula2/Conjuntos.cpp
@@ -1,150 +1,100 @@
-#include <stdio.h>
-
-
-struct Ler{
-
-   int num[10];
-   int junto[10];
-   int cont=0;
-   int aux=0;
-   int aux1=0;
-
-   void ler(){
-
-     printf("Digite os numeros:\n");
-     for(int i=0;i<10;i++){
-        scanf("%d",&num[i]);
-     }
-     printf("\n");
-
-   }
-
-   void tela(){
-
-        if(cont==0){
-           for(int i=0;i<10;i++){
-              junto[i]=num[i];
-           }
-           cont=1;
-        }else{
-           for(int i=0;i<10;i++){
-              aux=0;
-              aux1=0;
-              for(int j=0;j<10;j++){
-                 if(num[i]==junto[j]){
-                    aux++;
-                 }
-                 if(num[i]==num[j]){
-                     aux1++;
-                 }
-              }
-              if(aux==0 || aux1==1){
-                 printf("%d ",num[i]);
-              }
-           }
-        }
-
-    }
-
-};
-
+#include <iostream>
+#define TAM 20
 
 struct Conjunto{
-
+    
     int vetor[20];
-    int cont1=0;
-    int cont2=0;
-    int cont3=0;
-    int op;
-    int um, dois;
-
-    Ler tudo[20];
-
-    void criar(){
-
-       if(cont1==20){
-           printf("Nao existe mais espacos para conjuntos serem criados.\n\n");
-       }else{
-           cont1++;
-           vetor[cont1];
-
-           printf("O(s) seguinte(s) conjunto(s) criado(s): \n");
-           for(int i=0;i<cont1;i++){
-               printf("%d - ",i);
-           }
-           printf("\n\n");
-       }
-
-    }
-
+    int uni[20];
+    int a;
+    
     void ler(){
-
-       if(cont1==0){
-           printf("Nao ha o que ser preenchido.\n\n");
-       }else{
-          printf("Escolha um do(s) seguinte(s) conjunto(s) para ler:\n");
-          for(int i=0;i<cont1;i++){
-             printf("%d - ",i);
-          }
-          scanf("%d%*c",&op);
-          printf("\n");
-
-          tudo[op].ler();
-       }
-
-    }
-
-    void juntar(){
-
-        if(cont1<2){
-            printf("Nao ha conjuntos suficientes para a juncao.\n\n");
+        
+        printf("Digite quantos numeros ira digitar:\n");
+        scanf("%d",&a);
+        if(a>TAM){
+            printf("Capacidade insuficiente.");
         }else{
-
-            printf("Escolha 2 dos seguintes conjuntos para fazer a juncao:\n");
-            for(int i=0;i<cont1;i++){
-                printf("%d - ",i);
+            for(int i=0;i<a;i++){
+                scanf("%d",&vetor[i]);
+                
             }
-            scanf("%d %d",&um,&dois);
-
-            tudo[um].tela();
-            tudo[dois].tela();
-
-            printf("\n\n");
         }
+        printf("\n");
+        
+        
     }
-
+    
+    void imprimir(){
+            
+        for(int i=0;i<a;i++){
+            printf("%d ",vetor[i]);
+        }    
+        printf("\n");
+            
+    }
+    
+    void uniao(Conjunto dois){
+        
+        int j=0;
+        
+        if(a+dois.a>TAM){
+            
+            printf("%d\n\n\n\n\n\n",a+dois.a);
+            printf("Espaco insuficiente.\n");
+        }else{
+            for(int i=a;i<a+dois.a;i++){
+                vetor[i]=dois.vetor[j];
+                j++;
+            }
+            a=a+dois.a;
+            
+            for(int i=0;i<a;i++){
+                printf("%d ",vetor[i]);
+            }
+            printf("\n");
+        }
+        
+    }
+    
+    void inter(Conjunto dois){
+        
+        int x=0;
+        int in[TAM];
+        
+        printf("A intersecaoo dos 2 conjuntos:\n");
+        
+        for(int i=0;i<a;i++){
+            for(int j=0;j<dois.a;j++){
+                if(vetor[i]==dois.vetor[j]){
+                    in[x]=vetor[i];
+                    x++;
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0;i<x;i++){
+           vetor[i]=in[i];
+        }
+        for(int i=0;i<x/2;i++){
+           printf("%d ",vetor[i]);
+        }
+        
+    }
+ 
 };
 
-int main(){
+int main()
+{
+    
+    Conjunto um;
+    um.ler();
+    um.imprimir();
+    Conjunto dois;
+    dois.ler();
+    dois.imprimir();
+    um.uniao(dois);
+    um.inter(dois);
 
-
-  Conjunto tudo;
-
-  int op=1;
-
-  while(op!=0){
-
-    printf("Digite qual operacao deseja fazer: \n");
-    printf("Digite 1 - Criar um conjunto.\n");
-    printf("Digite 2 - Ler dados de um conjunto.\n");
-    printf("Digite 3 - Fazer a união de dois conjuntos.\n");
-    printf("Digite 4 - Fazer a interseção de dois conjuntos.\n");
-    printf("Digite 5 - Imprimir um conjunto.\n");
-    printf("Digite 0 - Fechar sistema.\n");
-    scanf("%d%*c",&op);
-
-    if(op==1){
-        printf("\n");
-        tudo.criar();
-    }else if(op==2){
-        printf("\n");
-        tudo.ler();
-    }else if(op==3){
-        printf("\n");
-        tudo.juntar();
-    }
-
-  }
-
-  return 0;
+    return 0;
 }
