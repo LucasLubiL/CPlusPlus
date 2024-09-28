00f5992a5059d573f86fe7e2d96711e882b46288
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2024-09-27 21:23:23 -0300

    Add files via upload

---
+++ StructAula2/Ponteiros/MinMax.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.14
+
+void ler(int *vetor,int *mi,int *ma,int *maior,int *menor){
+
+    if(*vetor>*maior){
+
+        *ma=*vetor;
+        *maior=*vetor;
+
+    }
+    if(*vetor<*menor){
+
+        *mi=*vetor;
+        *menor=*vetor;
+
+    }
+
+
+
+
+
+}
+
+int main(){
+
+
+   int maior=-10000;
+   int menor=10000;
+   int vetor[10];
+   int mi,ma;
+
+   for(int i=0;i<10;i++){
+       scanf("%d",&vetor[i]);
+       ler(&vetor[i],&mi,&ma,&maior,&menor);
+   }
+
+   printf("MAXIMO = %d\n",ma);
+   printf("MINIMO = %d\n",mi);
+
+
+
+  return 0;
+}
